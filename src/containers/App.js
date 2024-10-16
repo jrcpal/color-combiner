0f7458d2dbbbeeb16818dd99b53271fa62c0425d
Jessica Churgin <jchurgy@gmail.com>
2024-10-16 10:17:06 -0400

    update styles

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ColorPicker from '../features/ColorPicker';
 import ColorSearch from '../features/ColorSearch';
 import CombinedColorDisplay from '../features/CombinedColorDisplay';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <h1>Color Combiner</h1>
+    <div className="app-container">
+      <div className="header-container">
+        <h1>Color Combiner</h1>
+      </div>
       <ColorPicker />
       <ColorSearch />
       <CombinedColorDisplay />
